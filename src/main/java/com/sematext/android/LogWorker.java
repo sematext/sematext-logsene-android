e199a6c2c750d8c3bc19bec7a29c6124baebbe5b
Kim Low <kimhueilow@yahoo.com>
2019-03-15 16:41:14 -0700

    Move to WorkManager API
    Logsene used to start a background service to ship logs. However,
    starting Android 8, background apps are no longer allowed to start
    background services.
    https://developer.android.com/about/versions/oreo/background#services
    
    Based on Android guidelines, the recommended way is to use the
    WorkManager API. This patch removes how Logsene uses a background
    service to queue and ship logs. Instead, it schedule workers when logs
    need to be shipped.
    https://developer.android.com/topic/libraries/architecture/workmanager
    
    Changes:
    - Moved SqliteObjectQueue preflightQueue into Logsene.java, and added
      JSON objects directly into preflightQueue.
    - preflightQueue is now using Application context.
    - Deleted LogseneService.
    - Made LogWorker extend Worker and changed it to a one-off task.
    - Logsene.sendServiceIntent() is renamed to addToQueue().
    - Logsene.addToQueue() now checks if minimum batch size or time
      threshold has been hit, and if so, schedule a LogWorker work request
      with WorkManager, with some optional constraints such as only when
      charging, or only on unmetered network.
    - Added Logsene.flushMessageQueue(), so applications can flush the
      logs when it closes or just whenever.
    
    Logsene parameters added:
            <!-- Minimum batch size -->
            <meta-data
                android:name="LogseneMinBatchSize"
                android:value="500"
                />
            <!-- Flush logs at least every 10 minutes -->
            <meta-data
                android:name="LogseneMaxTimeTrigger"
                android:value="600000"
                />
            <!-- Set to true to send logs only on unmetered network -->
            <meta-data
                android:name="LogseneSendRequiresUnmeteredNetwork"
                android:value="true"
                />
            <!-- Set to true to send logs only when device is idle -->
            <meta-data
                android:name="LogseneSendRequiresDeviceIdle"
                android:value="false"
                />
            <!-- Set to true to send logs only when battery is not low -->
            <meta-data
                android:name="LogseneSendRequiresBatteryNotLow"
                android:value="true"
                />

--- src/main/java/com/sematext/android/LogWorker.java
+++ src/main/java/com/sematext/android/LogWorker.java
@@ -3,6 +3,7 @@ package com.sematext.android;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.support.annotation.NonNull;
 import android.util.Log;
 import com.sematext.android.logsene.ApiResponse;
 import com.sematext.android.logsene.Bulk;
@@ -12,162 +13,110 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.TimeUnit;
 
-class LogWorker {
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+public class LogWorker extends Worker {
   private final static String LOG_TAG = "Logsene";
 
   /**
-   * Queue for communicating with the appender thread.
+   * Maximum number of messages to send in one bulk request.
    */
-  private final BlockingQueue<JSONObject> queue = new LinkedBlockingQueue<>();
-  private final LogseneClient client;
-  private final String appToken;
-  private final String type;
-  private final int preflightSize;
-  private final Appender appender;
-  private final Context context;
+  private static final int MAX_BULK_SIZE = 50;
 
   /**
-   * Constructor.
-   *
-   * @param context application context
-   * @param client logsene client
-   * @param appToken the logsene app token
-   * @param type type to use when indexing messages
-   * @param preflightSize the max size of the preflight queue
-   * @throws IOException if unable to initialize preflight queue
+   * Number of times to attempt to send batch request.
    */
-  public LogWorker(Context context, LogseneClient client, String appToken, String type, int preflightSize)
-          throws IOException {
-    this.client = client;
-    this.appToken = appToken;
-    this.type = type;
+  private static final int MAX_ATTEMPTS = 3;
+
+  private LogseneClient client;
+  private String appToken;
+  private String type;
+  private final Context context;
+
+  private SqliteObjectQueue preflightQueue;
+
+  public LogWorker(@NonNull Context context, @NonNull WorkerParameters params) {
+    super(context, params);
     this.context = context;
-    this.preflightSize = preflightSize;
-
-    this.appender = new Appender();
-    Thread appenderThread = new Thread(appender);
-    appenderThread.start();
   }
 
-  public void addToQueue(JSONObject obj) {
-    this.queue.add(obj);
+  @Override
+  public Result doWork() {
+    appToken = getInputData().getString(Logsene.KEY_APPTOKEN);
+    type = getInputData().getString(Logsene.KEY_TYPE);
+
+    this.client = new LogseneClient(getInputData().getString(Logsene.KEY_RECEIVERURL), appToken);
+    this.preflightQueue = new SqliteObjectQueue(context, "logs");
+
+    long size = preflightQueue.size();
+    Log.d(LOG_TAG, "Worker started, message queue size = " + size);
+    if (size <= 0) {
+      return Result.success();
+    }
+
+    // sendInBatches() only returns false if nothing was sent
+    boolean success = sendInBatches();
+
+    if (!success) {
+        Log.e(LOG_TAG, "Worker failed to send logs");
+        return Result.failure();
+    } else {
+        Log.d(LOG_TAG, "Worker succeeded in sending logs, message queue size = " + preflightQueue.size());
+        return Result.success();
+    }
   }
 
-  private class Appender implements Runnable {
-    /**
-     * Minimum number of messages before sending request.
-     */
-    private static final int MIN_BATCH_SIZE = 10;
-
-    /**
-     * Maximum number of messages to send in one bulk request.
-     */
-    private static final int MAX_BULK_SIZE = 50;
-
-    /**
-     * Max time between sending requests.
-     */
-    private static final int MAX_TIME_TRIGGER = 60 * 1000;
-    // discuss: what happens if user opens the app -> exception is triggered & logged -> app is closed
-    // in this case we would never log the exception because batch size or max_time_trigger are not triggered.
-    // even if built as a long-running service that continues running after user closes the app, the user might
-    // delete the app out of frustration (that would stop the service)
-
-    /**
-     * Number of times to attempt to send batch request.
-     */
-    private static final int MAX_ATTEMPTS = 3;
-
-    /**
-     * Persistent queue for storing messages to be sent to Logsene.
-     *
-     * Messages are persisted as they are acknowledged, and they stay in the queue until they are successfully sent.
-     */
-    private final SqliteObjectQueue preflightQueue;
-
-    public Appender() throws IOException {
-      this.preflightQueue = new SqliteObjectQueue(context, "logs", preflightSize);
-    }
-
-    public void run() {
-      long lastSend = System.currentTimeMillis();
-
-      while(true) {
-        JSONObject newMessage;
-        try {
-          newMessage = queue.poll(1, TimeUnit.SECONDS);
-          if (newMessage != null) {
-            preflightQueue.add(newMessage);
-          }
-        } catch (InterruptedException e) {
-          return;
-        }
-        long sinceLastSend = System.currentTimeMillis() - lastSend;
-        if (isNetworkAvailable() && preflightQueue.size() > 0
-            && (preflightQueue.size() >= MIN_BATCH_SIZE || sinceLastSend > MAX_TIME_TRIGGER)) {
-          sendInBatches();
-          lastSend = System.currentTimeMillis();
-        }
+  private boolean sendInBatches() {
+    boolean success = false;
+    List<JSONObject> batch = preflightQueue.peek(MAX_BULK_SIZE);
+    do {
+      if (sendBatch(batch)) {
+        success = true;
+        preflightQueue.remove(batch.size());
+        batch = preflightQueue.peek(MAX_BULK_SIZE);
+      } else {
+        return success;
       }
-    }
+    } while (batch.size() > 0);
 
-    private void sendInBatches() {
-      List<JSONObject> batch = preflightQueue.peek(MAX_BULK_SIZE);
-      do {
-        if (sendBatch(batch)) {
-          preflightQueue.remove(batch.size());
-          batch = preflightQueue.peek(MAX_BULK_SIZE);
-        } else {
-          return;
-        }
-      } while (batch.size() > 0);
-    }
+    return success;
+  }
 
-    private boolean sendBatch(List<JSONObject> batch) {
-      Bulk.Builder bulkBuilder = new Bulk.Builder();
-      for (JSONObject obj : batch) {
-        bulkBuilder.addSource(obj.toString(), type);
-      }
-      return attemptExecute(bulkBuilder.build(), MAX_ATTEMPTS);
+  private boolean sendBatch(List<JSONObject> batch) {
+    Bulk.Builder bulkBuilder = new Bulk.Builder();
+    for (JSONObject obj : batch) {
+      bulkBuilder.addSource(obj.toString(), type);
     }
+    return attemptExecute(bulkBuilder.build(), MAX_ATTEMPTS);
+  }
 
-    private boolean attemptExecute(Bulk bulk, int leftAttempts) {
-      if (leftAttempts == 0) {
+  private boolean attemptExecute(Bulk bulk, int leftAttempts) {
+    if (leftAttempts == 0) {
+      return false;
+    }
+    leftAttempts -= 1;
+    try {
+      Log.d(LOG_TAG, "Attempting to send bulk request");
+      ApiResponse result = client.execute(bulk);
+      if (!result.isSuccessful()) {
+        Log.e(LOG_TAG, String.format("Bad status code (%d) returned from api. Response: %s",
+                result.getHttpResponse().code(), result.getBody()));
         return false;
-      }
-      leftAttempts -= 1;
-      try {
-        Log.d(LOG_TAG, "Attempting to send bulk request");
-        ApiResponse result = client.execute(bulk);
-        if (!result.isSuccessful()) {
-          Log.e(LOG_TAG, String.format("Bad status code (%d) returned from api. Response: %s",
-                  result.getHttpResponse().code(), result.getBody()));
-          return false;
-        } else {
-          // even though the status code is successful, some documents might have failed
-          JSONObject json = result.getJson();
-          if (json != null && Boolean.parseBoolean(json.optString("errors", "false"))) {
-            // we just log the error, as we most likely cannot resolve the issue by retrying these documents
-            Log.e(LOG_TAG, String.format("Unable to index all documents. Response: %s\nRequest: %s",
-                    result.getBody(), bulk.toString(appToken)));
-          }
+      } else {
+        // even though the status code is successful, some documents might have failed
+        JSONObject json = result.getJson();
+        if (json != null && Boolean.parseBoolean(json.optString("errors", "false"))) {
+          // we just log the error, as we most likely cannot resolve the issue by retrying these documents
+          Log.e(LOG_TAG, String.format("Unable to index all documents. Response: %s\nRequest: %s",
+                  result.getBody(), bulk.toString(appToken)));
         }
-        return true;
-      } catch (IOException e) {
-        Log.e(LOG_TAG, "Error while sending logs", e);
-        return attemptExecute(bulk, leftAttempts);
       }
-    }
-
-    private boolean isNetworkAvailable() {
-      ConnectivityManager connectivityManager =
-              (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-      NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
-      return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+      return true;
+    } catch (IOException e) {
+      Log.e(LOG_TAG, "Error while sending logs", e);
+      return attemptExecute(bulk, leftAttempts);
     }
   }
 }
--- src/main/java/com/sematext/android/Logsene.java
+++ src/main/java/com/sematext/android/Logsene.java
@@ -5,27 +5,138 @@ import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Build;
+import android.os.Bundle;
+import android.os.SystemClock;
 import android.util.Log;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.Iterator;
+import java.util.concurrent.TimeUnit;
+
+import androidx.work.Constraints;
+import androidx.work.Data;
+import androidx.work.ExistingWorkPolicy;
+import androidx.work.NetworkType;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkManager;
 
 /**
  * Logs messages to Logsene from Android applications.
  */
 public class Logsene {
+  public static final String KEY_RECEIVERURL = "LOGSENE_RECEIVERURL";
+  public static final String KEY_APPTOKEN = "LOGSENE_APPTOKEN";
+  public static final String KEY_TYPE = "LOGSENE_TYPE";
+
   private final String TAG = getClass().getSimpleName();
+  private final String UNCONSTRAINED_WORKER_TAG = "com.sematext.android.LogWorker.unconstrained";
+  private final String CONSTRAINED_WORKER_TAG = "com.sematext.android.LogWorker.constrained";
+
+  private final String RECEIVER_URL = "https://logsene-receiver.sematext.com";
+
+  /**
+   * Maximum number of messages to cache when offline.
+   */
+  private final int DEFAULT_MAX_OFFLINE_MESSAGES = 5000;
+
+  /**
+   * Minimum number of messages before sending request.
+   */
+  private final int DEFAULT_MIN_BATCH_SIZE = 10;
+
+  /**
+   * Max time between sending requests.
+   */
+  private static final int DEFAULT_MAX_TIME_TRIGGER = 60 * 1000;
+  // discuss: what happens if user opens the app -> exception is triggered & logged -> app is closed
+  // in this case we would never log the exception because batch size or max_time_trigger are not triggered.
+  // even if built as a long-running service that continues running after user closes the app, the user might
+  // delete the app out of frustration (that would stop the service)
+
   private static JSONObject defaultMeta;
   private final Context context;
   private String versionName;
   private Integer versionCode;
   private String uuid;
 
+  /**
+   * Persistent queue for storing messages to be sent to Logsene.
+   *
+   * Messages are persisted as they are acknowledged.
+   * Messages are dequeued from LogWorker, and only removed when they are successfully sent.
+   */
+  private SqliteObjectQueue preflightQueue;
+  private long lastScheduled;
+
+  private String appToken;
+  private String type;
+  private String receiverUrl;
+  private int maxOfflineMessages;
+  private int minBatchSize;
+  private long maxTimeTrigger;
+  private boolean sendRequiresUnmeteredNetwork;
+  private boolean sendRequiresDeviceIdle;
+  private boolean sendRequiresBatteryNotLow;
+
   public Logsene(Context context) {
     Utils.requireNonNull(context);
     this.context = context;
     this.uuid = Installation.id(context);
+    config();
+    // Database initialization can potentially take a long time, and Logsene can be initialized
+    // in UI thread, hence spawn a separate thread
+    Thread initDbThread = new Thread("Initialize Logsene database") {
+      public void run() {
+        Logsene.this.preflightQueue = new SqliteObjectQueue(Logsene.this.context, "logs", maxOfflineMessages);
+      }
+    };
+    initDbThread.start();
+    this.lastScheduled = SystemClock.elapsedRealtime();
+  }
+
+  public long getQueueSize() {
+    return preflightQueue.size();
+  }
+
+  private void config() {
+    Bundle data = null;
+    try {
+      data = context.getPackageManager().getApplicationInfo(context.getPackageName(),
+            PackageManager.GET_META_DATA).metaData;
+    } catch (PackageManager.NameNotFoundException e) {
+      throw new RuntimeException(e);
+    }
+
+    // required fields
+    if (!data.containsKey("LogseneAppToken")) {
+      throw new RuntimeException("Please provide <meta-data name=\"LogseneAppToken\" value=\"yourapptoken\">");
+    } else if (!data.containsKey("LogseneType")) {
+      throw new RuntimeException("Please provide <meta-data name=\"LogseneType\" value=\"example\">");
+    }
+    appToken = data.getString("LogseneAppToken");
+    type = data.getString("LogseneType");
+
+    // optional fields
+    receiverUrl = data.getString("LogseneReceiverUrl", RECEIVER_URL);
+    maxOfflineMessages = data.getInt("LogseneMaxOfflineMessages", DEFAULT_MAX_OFFLINE_MESSAGES);
+    minBatchSize = data.getInt("LogseneMinBatchSize", DEFAULT_MIN_BATCH_SIZE);
+    maxTimeTrigger = (long)(data.getInt("LogseneMaxTimeTrigger", DEFAULT_MAX_TIME_TRIGGER));
+    sendRequiresUnmeteredNetwork = data.getBoolean("LogseneSendRequiresUnmeteredNetwork", false);
+    sendRequiresDeviceIdle = data.getBoolean("LogseneSendRequiresDeviceIdle", false);
+    sendRequiresBatteryNotLow = data.getBoolean("LogseneSendRequiresBatteryNotLow", false);
+
+    Log.d(TAG, String.format("Logsene is configured:\n"
+        + "  Type:                                   %s\n"
+        + "  Receiver URL:                           %s\n"
+        + "  Max Offline Messages:                   %d\n"
+        + "  Min Batch Size:                         %d\n"
+        + "  Max Time Trigger:                       %d\n"
+        + "  Send logs only on unmetered network:    %s\n"
+        + "  Send logs only when device is idle:     %s\n"
+        + "  Send logs only when battery is not low: %s",
+        type, receiverUrl, maxOfflineMessages, minBatchSize, maxTimeTrigger,
+        sendRequiresUnmeteredNetwork, sendRequiresDeviceIdle, sendRequiresBatteryNotLow));
   }
 
   /**
@@ -40,7 +151,7 @@ public class Logsene {
     try {
       obj.put("level", level);
       obj.put("message", message);
-      sendServiceIntent(obj);
+      addToQueue(obj);
     } catch (JSONException e) {
       // thrown when key is null in put(), so should never happen
       Log.e(TAG, "Failed to construct json object", e);
@@ -108,7 +219,7 @@ public class Logsene {
       obj.put("exception", error.getClass().toString());
       obj.put("message", error.getMessage());
       obj.put("stacktrace", Utils.getStackTrace(error));
-      sendServiceIntent(obj);
+      addToQueue(obj);
     } catch (JSONException e) {
       // thrown when key is null in put(), so should never happen
       Log.e(TAG, "Failed to construct json object", e);
@@ -121,7 +232,7 @@ public class Logsene {
    */
   public void event(JSONObject object) {
     Utils.requireNonNull(object);
-    sendServiceIntent(object);
+    addToQueue(object);
   }
 
   /**
@@ -135,12 +246,99 @@ public class Logsene {
     defaultMeta = metadata;
   }
 
-  private void sendServiceIntent(JSONObject obj) {
+  /**
+   * Flush the message queue.
+   *
+   * This call is optional, but it is recommended to be called when application is destroyed.
+   */
+  public void flushMessageQueue() {
+    Log.d(TAG, "Flushing message queue, message queue size = " + preflightQueue.size());
+    scheduleUnconstrainedWorker();
+  }
+
+  private void scheduleUnconstrainedWorker() {
+    Data workerData = new Data.Builder()
+        .putString(KEY_RECEIVERURL, receiverUrl)
+        .putString(KEY_APPTOKEN, appToken)
+        .putString(KEY_TYPE, type)
+        .build();
+
+    OneTimeWorkRequest workRequest = new OneTimeWorkRequest.Builder(LogWorker.class)
+        .addTag(UNCONSTRAINED_WORKER_TAG)
+        .setInputData(workerData)
+        .build();
+
+    WorkManager.getInstance().enqueueUniqueWork(UNCONSTRAINED_WORKER_TAG,
+        ExistingWorkPolicy.KEEP, workRequest);
+  }
+
+  private void scheduleConstrainedWorker() {
+    Data workerData = new Data.Builder()
+        .putString(KEY_RECEIVERURL, receiverUrl)
+        .putString(KEY_APPTOKEN, appToken)
+        .putString(KEY_TYPE, type)
+        .build();
+
+    Constraints workerConstraints = new Constraints.Builder()
+        .setRequiredNetworkType(
+            sendRequiresUnmeteredNetwork ? NetworkType.UNMETERED : NetworkType.CONNECTED)
+        .setRequiresDeviceIdle(sendRequiresDeviceIdle)
+        .setRequiresBatteryNotLow(sendRequiresBatteryNotLow)
+        .build();
+
+    OneTimeWorkRequest workRequest = new OneTimeWorkRequest.Builder(LogWorker.class)
+        .addTag(CONSTRAINED_WORKER_TAG)
+        .setInputData(workerData)
+        .setConstraints(workerConstraints)
+        .build();
+
+    WorkManager.getInstance().enqueueUniqueWork(CONSTRAINED_WORKER_TAG,
+        ExistingWorkPolicy.KEEP, workRequest);
+  }
+
+  private void addToQueue(JSONObject obj) {
+    if (preflightQueue == null) {
+      Log.e(TAG, "Message queue has not been initialized, message dropped.");
+      return;
+    }
+
     assert obj != null;
     enrich(obj);
-    Intent intent = new Intent(context, LogseneService.class);
-    intent.putExtra("obj", obj.toString());
-    context.startService(intent);
+
+    preflightQueue.add(obj);
+    if (preflightQueue.size() > maxOfflineMessages) {
+      Log.e(TAG, "Message queue overflowed (" + preflightQueue.size() + " > "
+          + maxOfflineMessages + "), some logs are lost.");
+    }
+
+    // Approaching message queue limit, schedule a one-time unconstrained worker
+    if (preflightQueue.size() >= (maxOfflineMessages * 0.9)) {
+      scheduleUnconstrainedWorker();
+
+      lastScheduled = SystemClock.elapsedRealtime();
+      //Log.d(TAG, "Unconstrained worker enqueued, message queue size = " + preflightQueue.size());
+
+    // Schedule constrained worker if minimum batch size met, or the time from last scheduled
+    // exceeds maxTimeTrigger.
+    // The time threshold is used to account for the case where the message queue stays below
+    // the minBatchSize for a long time.
+    } else if (preflightQueue.size() >= minBatchSize
+        || (SystemClock.elapsedRealtime() - lastScheduled) > maxTimeTrigger) {
+      scheduleConstrainedWorker();
+
+      lastScheduled = SystemClock.elapsedRealtime();
+      //Log.d(TAG, "Constrained worker enqueued, message queue size = " + preflightQueue.size());
+    }
+
+    /*
+    try {
+        Log.d(TAG, "Unconstrained Worker is "
+            + WorkManager.getInstance().getWorkInfosForUniqueWork(UNCONSTRAINED_WORKER_TAG).get().get(0).getState());
+        Log.d(TAG, "Constrained Worker is "
+            + WorkManager.getInstance().getWorkInfosForUniqueWork(CONSTRAINED_WORKER_TAG).get().get(0).getState());
+    } catch (Exception e) {
+    }
+    */
   }
 
   private String getVersionName() {
